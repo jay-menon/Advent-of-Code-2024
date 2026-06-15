8e4d92fe59af3ee85106b3868e1874986da5bc53
Vijay Menon <vijaymenon14@outlook.com>
2026-06-16 00:12:00 +0100

    Finish Day 1 pt. 1 solution

--- Day_1.py
+++ Day_1.py
@@ -4,16 +4,18 @@
 # ///////// CLEANING PUZZLE INPUT /////////
 with open("d1_input.txt") as raw_input:
     raw_ranges = raw_input.readlines()
-
-# ////////////////// PART 1 //////////////////
 stripped_input = [i.strip("\n") for i in raw_ranges]
 clean_input = [i.split("   ") for i in stripped_input]
 int_input = [[int(i[0]), int(i[1])] for i in clean_input]
-print(int_input)
+# print(int_input)
 
-
-
-
-
-
-
+# ////////////////// PART 1 //////////////////
+# Sort each list by order of their distances
+lst1 = sorted([i[0] for i in int_input])
+lst2 = sorted([i[1] for i in int_input])
+comb_list = zip(lst1, lst2)
+# Sum the abs. difference between the same ranked distances in each list
+sum = 0
+for i in comb_list:
+    sum += abs(i[0]-i[1])
+print("PART 1 /// Sum of Differences in Distances: " + str(sum))
