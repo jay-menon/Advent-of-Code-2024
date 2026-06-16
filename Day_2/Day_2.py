abbdd7c60c6f1ae1f4b59ed564cb9f6f8cac72ca
Vijay Menon <vijaymenon14@outlook.com>
2026-06-16 02:48:16 +0100

    Complete pt.2 submission

--- Day_2/Day_2.py
+++ Day_2/Day_2.py
@@ -38,3 +38,32 @@ print("PART 1 /// Freq. of Safe Reports: " + str(safe_count))
 
 # ////////////////// PART 2 //////////////////
 
+# Returns 1 if the dampener made the unsafe report safe, else, returns 0
+def dampener(unsafe_report):
+    def rule1(report):
+        level_copy = list(report)
+        level_copy.sort()
+        if report == level_copy or report == list(reversed(level_copy)):
+            return True
+        return False
+    def rule2(report):
+        prev_level = report[0]
+        for i in report[1:]:
+            curr_level = i
+            diff = abs(prev_level - curr_level)
+            if diff < 1 or diff > 3:
+                return False
+            prev_level = i
+        return True
+    # Take out all levels individually and check if rules now met
+    for i in range(0,len(unsafe_report)):
+        copy = list(unsafe_report)
+        copy.pop(i)
+        if rule1(copy) is True and rule2(copy) is True:
+            return 1
+    return 0
+
+# Check how many of the unsafe reports have now become safe
+for i in unsafe_reports:
+    safe_count += dampener(i)
+print("PART 2 /// New Freq. of Safe Reports: " + str(safe_count))
