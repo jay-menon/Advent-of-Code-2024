b889b8462cdef69957ee6e57e9cd840908758b4e
Vijay Menon <vijaymenon14@outlook.com>
2026-06-17 02:28:35 +0100

    Complete pt.1 submission

--- Day_3/Day_3.py
+++ Day_3/Day_3.py
@@ -4,8 +4,35 @@
 # ///////// CLEANING PUZZLE INPUT /////////
 with open("d3_input.txt") as raw_input:
     raw_code_list = raw_input.readlines()
-# Note that raw_code_list
-print(len(raw_code_list))
+# Note that raw_code_list is a 6 item list of large blocks of code
+
+# ////////////////// PART 1 //////////////////
+# Parses code for m and when found, will check for correct mul formatting
+# Also removes all code before+including m to shorten next search
+def mul_finder(code):
+    # Finds m and removes all before m, including the m
+    # Including m so same m doesn't get picked up on next parse
+    m_idx = code.index("m")
+    trunc_code = code[m_idx+1:]
+    # Testing formatting of the potential mul
+    com_idx = trunc_code.index(",")
+    brack_idx = trunc_code.index(")")
+    if trunc_code[0:3] == "ul(" and com_idx < brack_idx:
+        test_list = trunc_code[3:brack_idx].split(",")
+        if len(test_list) == 2 and test_list[0].isdigit() and test_list[1].isdigit():
+            return [[int(test_list[0]), int(test_list[1])], trunc_code]
+    return [False, trunc_code]
+
+sum = 0
+for code in raw_code_list:
+    curr_code = code
+    for m in range(0,curr_code.count("m")):
+        result = mul_finder(curr_code)
+        if result[0] is not False:
+            sum += result[0][0] * result[0][1]
+        curr_code = result[1]
+
+print("PART 1 /// Sum of Correctly Formatted Factors: " + str(sum))
 
 # Overall strategy
 # Take big input and use index to find an m
