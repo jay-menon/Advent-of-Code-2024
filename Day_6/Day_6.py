8f258c35ae2fc48e0eb876d565ed6f14313f7b21
Vijay Menon <vijaymenon14@outlook.com>
2026-06-30 20:15:57 +0100

    Draft of path range arithmetic method: switch to position counting from now on

--- Day_6/Day_6.py
+++ Day_6/Day_6.py
@@ -20,7 +20,6 @@ horz_map = [
     "......#..."
 ]
 
-
 vert_map = []
 for col_idx in range(0, len(horz_map[0])):
     new_str = ""
@@ -50,13 +49,13 @@ def knight_tracker(pos, ori, horz_map, vert_map):
     elif ori == 2:
         path = vert_map[pos[0]][pos[1]:]
         if path.count("#") == 0:
-            return "Complete", 3, [[pos[0], pos[0]], [pos[1], len(vert_map[0])]] 
+            return "Complete", 3, [[pos[0], pos[0]], [pos[1], len(vert_map[0])-1]] 
         else:
             return [pos[0], pos[1]+path.index("#")-1], 3, [[pos[0], pos[0]], [pos[1], pos[1]+path.index("#")-1]]
     elif ori == 1:
         path = horz_map[pos[1]][pos[0]:]
         if path.count("#") == 0:
-            return "Complete", 2, [[pos[0], len(horz_map[0])], [pos[1], pos[1]]]
+            return "Complete", 2, [[pos[0], len(horz_map[0])-1], [pos[1], pos[1]]]
         else:
             return [pos[0]+path.index("#")-1, pos[1]], 2, [[pos[0], pos[0]+path.index("#")-1], [pos[1], pos[1]]]
     elif ori == 3:
@@ -66,17 +65,34 @@ def knight_tracker(pos, ori, horz_map, vert_map):
         else:
             return [pos[0]-path[::-1].index("#"), pos[1]], 0, [[pos[0], pos[0]-path[::-1].index("#")], [pos[1], pos[1]]]
 
+def new_path_count(test_range, range_list):
+    overlap_count = 0
+    if test_range[0][0] == test_range[0][1]:
+        range_len = abs(test_range[1][0]-test_range[1][1])+1
+        for old_range in range_list:
+            if old_range[0][0] != old_range[0][1] and test_range[0][0] >= min(old_range[0][0],old_range[0][1]) and test_range[0][0] <= max(old_range[0][0],old_range[0][1]):
+                if old_range[1][0] >= min(test_range[1][0],test_range[1][1]) and old_range[1][0] <= max(test_range[1][0],test_range[1][1]):
+                    overlap_count += 1
+    else:
+        range_len = abs(test_range[0][0]-test_range[0][1])+1
+        for old_range in range_list:
+            if old_range[1][0] != old_range[1][1] and test_range[1][0] >= min(old_range[1][0],old_range[1][1]) and test_range[1][0] <= max(old_range[1][0],old_range[1][1]):
+                if old_range[0][0] >= min(test_range[1][0],test_range[1][1]) and old_range[0][0] <= max(test_range[1][0],test_range[1][1]):
+                    overlap_count += 1
+    return range_len - overlap_count
 
 # Testing
 initial_pos = init_pos(horz_map)
 #print(knight_tracker(init_pos, 0, horz_map, vert_map))
 
+ground_covered = 0
 curr_pos = initial_pos
 curr_ori = 0
 path_range_list = []
 while curr_pos != "Complete":
     [curr_pos, curr_ori, path_range] = knight_tracker(curr_pos, curr_ori, horz_map, vert_map)
     print(curr_pos)
+    ground_covered += new_path_count(path_range, path_range_list)
     path_range_list.append(path_range)
-
+print(ground_covered)
 
