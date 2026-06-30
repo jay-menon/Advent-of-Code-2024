0f59d628338bfb8c532b5931df248a376b70731c
Vijay Menon <vijaymenon14@outlook.com>
2026-06-30 20:20:09 +0100

    Complete Day_6 pt.1 submission

--- Day_6/Day_6.py
+++ Day_6/Day_6.py
@@ -6,20 +6,6 @@ with open("d6_input.txt") as raw_input:
     raw_map = raw_input.readlines()
 
 horz_map = [i.strip("\n") for i in raw_map]
-
-horz_map = [
-    "....#.....",
-    ".........#",
-    "..........",
-    "..#.......",
-    ".......#..",
-    "..........",
-    ".#..^.....",
-    "........#.",
-    "#.........",
-    "......#..."
-]
-
 vert_map = []
 for col_idx in range(0, len(horz_map[0])):
     new_str = ""
@@ -36,7 +22,6 @@ def init_pos(map):
             return [line.index("^"), line_idx]
         line_idx += 1
     return "No initial pos found"
-
 # Function takes knight's current position and outputs final pos, final ori(after 90deg) and range of the path taken
 # If knight moves off map, function returns "Complete"
 def knight_tracker(pos, ori, horz_map, vert_map):
@@ -64,35 +49,30 @@ def knight_tracker(pos, ori, horz_map, vert_map):
             return "Complete", 0, [[pos[0], 0], [pos[1], pos[1]]]
         else:
             return [pos[0]-path[::-1].index("#"), pos[1]], 0, [[pos[0], pos[0]-path[::-1].index("#")], [pos[1], pos[1]]]
-
-def new_path_count(test_range, range_list):
-    overlap_count = 0
-    if test_range[0][0] == test_range[0][1]:
-        range_len = abs(test_range[1][0]-test_range[1][1])+1
-        for old_range in range_list:
-            if old_range[0][0] != old_range[0][1] and test_range[0][0] >= min(old_range[0][0],old_range[0][1]) and test_range[0][0] <= max(old_range[0][0],old_range[0][1]):
-                if old_range[1][0] >= min(test_range[1][0],test_range[1][1]) and old_range[1][0] <= max(test_range[1][0],test_range[1][1]):
-                    overlap_count += 1
+# Function takes a range of positions and ouputs each individual position visited as a list
+def range_to_pos(t_range):
+    pos_list = []
+    if t_range[0][0] == t_range[0][1]:
+        for i in range(min(t_range[1]), max(t_range[1])+1):
+            pos_list.append([t_range[0][0],i])
     else:
-        range_len = abs(test_range[0][0]-test_range[0][1])+1
-        for old_range in range_list:
-            if old_range[1][0] != old_range[1][1] and test_range[1][0] >= min(old_range[1][0],old_range[1][1]) and test_range[1][0] <= max(old_range[1][0],old_range[1][1]):
-                if old_range[0][0] >= min(test_range[1][0],test_range[1][1]) and old_range[0][0] <= max(test_range[1][0],test_range[1][1]):
-                    overlap_count += 1
-    return range_len - overlap_count
+        for i in range(min(t_range[0]), max(t_range[0])+1):
+            pos_list.append([i, t_range[1][0]])
+    return pos_list
+# Function only adds new positions visited to the old position list
+def consol_lists(old_pos_vis, pos_list):
+    new_pos_list = list(old_pos_vis)
+    for pos in pos_list:
+        if pos not in old_pos_vis:
+            new_pos_list.append(pos)
+    return new_pos_list
 
-# Testing
-initial_pos = init_pos(horz_map)
-#print(knight_tracker(init_pos, 0, horz_map, vert_map))
-
-ground_covered = 0
-curr_pos = initial_pos
+curr_pos = init_pos(horz_map)
 curr_ori = 0
-path_range_list = []
+curr_pos_list = []
 while curr_pos != "Complete":
     [curr_pos, curr_ori, path_range] = knight_tracker(curr_pos, curr_ori, horz_map, vert_map)
-    print(curr_pos)
-    ground_covered += new_path_count(path_range, path_range_list)
-    path_range_list.append(path_range)
-print(ground_covered)
+    pos_list = range_to_pos(path_range)
+    curr_pos_list = consol_lists(curr_pos_list, pos_list)
+print(len(curr_pos_list))
 
