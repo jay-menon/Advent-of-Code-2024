f40f1685a9e5ceeb56685e5e6a79c80f6ee16f1e
Vijay Menon <vijaymenon14@outlook.com>
2026-06-30 17:56:47 +0100

    Complete Day_5 pt.2 submission

--- Day_5/Day_5.py
+++ Day_5/Day_5.py
@@ -24,13 +24,57 @@ def rule_tester(str_rule, str_update):
     return False
 
 sum = 0
+upd_to_fix_list = []
 for update in str_upd_list:
     rule_broken = False
     rel_rules = rule_finder(str_rule_list, update)
     for rule in rel_rules:
         if rule_tester(rule, update) is False:
             rule_broken = True
+            upd_to_fix_list.append(update)
             break
     if rule_broken is False:
         sum += int(update[int((len(update)-1)/2)])
 print("Sum of Ordered Update Middle Nums: " + str(sum))
+
+# ////////////////// PART 2 //////////////////
+# Takes in an update and a set of rules, outputs the furthest left number
+def left_num(update, rule_list):
+    for update_num in update:
+        not_furth_left = False
+        for rule in rule_list:
+            if rule[1] == update_num:
+                not_furth_left = True
+                break
+        if not_furth_left is False:
+            return update_num
+# Takes in a number, rules list and update, outputs all rule list and update without that number
+def rule_strip(num, update, rules):
+    new_rules = []
+    for rule in rules:
+        if num not in rule:
+            new_rules.append(rule)
+    new_upd = list(update)
+    new_upd.remove(num)
+    return [new_upd, new_rules]
+# Finds middle index item in an odd length list:
+def mid_item(list_arg):
+    if len(list_arg) % 2 == 1:
+        return list_arg[int((len(list_arg)-1)/2)]
+    else:
+        return "List is even lengthed"
+
+sum = 0
+for update in upd_to_fix_list:
+    rel_rules = rule_finder(str_rule_list, update)
+    fixed_update = []
+    [curr_update, curr_rules] = [update, rel_rules]
+
+    while len(fixed_update) != len(update):
+        leftmost = left_num(curr_update, curr_rules)
+        fixed_update.append(leftmost)
+        [curr_update, curr_rules] = rule_strip(leftmost, curr_update, curr_rules)
+
+    mid_num = int(mid_item(fixed_update))
+    sum += mid_num
+print("Sum of Fixed Ordered Update Middle Nums: " + str(sum))
